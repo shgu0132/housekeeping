a6a9dc88f1256ee265a36dd43976932795eaa420
shgu0132 <g.shivendra@gmail.com>
2022-03-08 00:15:55 +0100

    included option for capacity also

--- backend/windows/fs_functions.ps1
+++ backend/windows/fs_functions.ps1
@@ -3,7 +3,14 @@
 # Usage: /bin/bash ./fs_functions.sh "path" 0|1
 # 0 indicates that path is non-root directory and 1 indicates that path is a root directory
 
-$dirPath="\\EC2AMAZ-V9Q88DH\C$\Boot"
+ # this script generates below details about the path passed as a parameter
+# Path,Size,No Of Files,No of Directories,percent of Parent,Last Modified,Last Accessed,Owner
+# Usage: /bin/bash ./fs_functions.sh "path" 0|1
+# 0 indicates that path is non-root directory and 1 indicates that path is a root directory
+
+# $dirPath="\\EC2AMAZ-V9Q88DH\C$\Boot"
+$dirPath=$Args[0]
+$isRoot=$Args[1]
 $driveletters=@("D","E","F","G","H","I","J","K","L","N","O","P","Q","R","S","T","U","V","W")
 $nwobj=New-Object -comobject WScript.Network
 ForEach($driveLetter in $driveletters){
@@ -25,32 +32,33 @@ $total=($free+$used)
 # $freewithoutdecimal=([math]::Round($freeinPercentage))
 # $totalGB=[math]::Round($total/1gb)
 # Write-Output "Share $dirPath has total space of $total bytes, free=$free bytes, used=$used bytes"
-$size=$total
 $status=$nwobj.removenetworkdrive($qualifier)
 # $status=Remove-PSDrive $driveLetter 
 
-if ( (Get-Item $dirPath) -is [System.IO.DirectoryInfo] )    {
-    $nFiles=(Get-ChildItem -Recurse -File | Measure-Object).Count
-    $nDirectories=(Get-ChildItem -Recurse -Directory | Measure-Object).Count
-}
-elseif ( (Get-Item $dirPath) -is [System.IO.FileInfo] ) )  {
+if ( (Test-Path -Path $dirPath -PathType Leaf) )    {
     $nFiles=1
-    $nDirectories=(Get-ChildItem -Recurse -Directory | Measure-Object).Count
+    $nDirectories=0
+}
+elseif ( (Test-Path -Path $dirPath -PathType Container) )  {
+    $nFiles=(Get-ChildItem -Recurse -File -Path $dirPath | Measure-Object).Count
+    $nDirectories=(Get-ChildItem -Recurse -Directory -Path $dirPath | Measure-Object).Count
 }
 else    {
     echo "$dirPath is not valid"
     exit 1
 }
+$size=(Get-ChildItem $dirPath -Recurse | Measure-Object Length -Sum).Sum
 if ( $isRoot -eq 1 )    {
-    percentParent="100%"
+    $percentParent="100%"
 }
 else    {
-    $pSize=(Get-ChildItem $dirPath -Recurse | Measure-Object Length -Sum).Sum
+    $pSize=(Get-ChildItem $dirPath/.. -Recurse | Measure-Object Length -Sum).Sum
     echo $size,$pSize
     $percentParent=(100 * $size/$pSize )
 }
 $lastModified=(Get-Item -Path $dirPath).LastWriteTime
 $lastAccessed=(Get-Item -Path $dirPath).LastAccessTime 
 $owner=(Get-ACL -Path $dirPath).Owner
-echo Path,Size,No Of Files,No of Directories,percent of Parent,Last Modified,Last Accessed,Owner
-echo $dirPath,$size,$nFiles,$nDirectories,$percentParent,$lastModified,$lastAccessed,$owner
+echo "Capacity,Path,Size,No Of Files,No of Directories,percent of Parent,Last Modified,Last Accessed,Owner"
+echo "$total,$dirPath,$size,$nFiles,$nDirectories,$percentParent,$lastModified,$lastAccessed,$owner"
+ 
