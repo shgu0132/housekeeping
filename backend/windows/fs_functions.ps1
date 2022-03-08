6a62b798af3e17e2fa1b0e9d8d0b6d91c3c50f68
shgu0132 <g.shivendra@gmail.com>
2022-03-08 10:27:22 +0100

    Update fs_functions.ps1
    added flexibility for providing UNC path or direct path

--- backend/windows/fs_functions.ps1
+++ backend/windows/fs_functions.ps1
@@ -11,30 +11,39 @@
 # $dirPath="\\EC2AMAZ-V9Q88DH\C$\Boot"
 $dirPath=$Args[0]
 $isRoot=$Args[1]
-$driveletters=@("D","E","F","G","H","I","J","K","L","N","O","P","Q","R","S","T","U","V","W")
-$nwobj=New-Object -comobject WScript.Network
-ForEach($driveLetter in $driveletters){
-	if (Get-PSDrive $driveLetter -ErrorAction SilentlyContinue) {
-    	Continue
-	} else {
-    	$status=New-PSDrive -Name $driveLetter -PSProvider FileSystem -Root $dirPath
-        $qualifier = $driveletter + ":"
-		$status=$nwobj.mapnetworkdrive($qualifier,$dirPath)
-        Break
-	}
+$pathType=$dirPath.substring(0, 2)
+if ($pathType -like '\\')   {
+    $driveletters=@("D","E","F","G","H","I","J","K","L","N","O","P","Q","R","S","T","U","V","W")
+    $nwobj=New-Object -comobject WScript.Network
+    ForEach($driveLetter in $driveletters){
+        if (Get-PSDrive $driveLetter -ErrorAction SilentlyContinue) {
+            Continue
+        } else {
+            # $status=New-PSDrive -Name $driveLetter -PSProvider FileSystem -Root $dirPath
+            $qualifier = $driveletter + ":"
+            $status=$nwobj.mapnetworkdrive($qualifier,$dirPath)
+            Break
+        }
+    }
+    # echo $driveLetter,$drive
+    $drive=Get-PSdrive $driveLetter
+    $free=($drive.free)
+    $used=($drive.used)
+    $total=($free+$used)
+    # $freeinPercentage=($free/$total*100)
+    # $freewithoutdecimal=([math]::Round($freeinPercentage))
+    # $totalGB=[math]::Round($total/1gb)
+    # Write-Output "Share $dirPath has total space of $total bytes, free=$free bytes, used=$used bytes"
+    $status=$nwobj.removenetworkdrive($qualifier)
+    # $status=Remove-PSDrive $driveLetter 
 }
-# echo $driveLetter,$drive
-$drive=Get-PSdrive $driveLetter
-$free=($drive.free)
-$used=($drive.used)
-$total=($free+$used)
-# $freeinPercentage=($free/$total*100)
-# $freewithoutdecimal=([math]::Round($freeinPercentage))
-# $totalGB=[math]::Round($total/1gb)
-# Write-Output "Share $dirPath has total space of $total bytes, free=$free bytes, used=$used bytes"
-$status=$nwobj.removenetworkdrive($qualifier)
-$status=Remove-PSDrive $driveLetter 
-
+else if ($pathType -like '?:')   {
+    $driveLetter = $pathType
+    $drive=Get-PSdrive $driveLetter
+    $free=($drive.free)
+    $used=($drive.used)
+    $total=($free+$used)    
+}
 if ( (Test-Path -Path $dirPath -PathType Leaf) )    {
     $nFiles=1
     $nDirectories=0
